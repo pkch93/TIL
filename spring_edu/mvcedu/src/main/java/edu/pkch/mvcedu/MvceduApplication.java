a080f98a171b397be81ec8190528f6d2eb61c683
pkch93 <star_final@naver.com>
2019-07-14 00:16:33 +0900

    TIL | 20190714 0016 AM | Spring MVC edu setting

---
+++ spring_edu/mvcedu/src/main/java/edu/pkch/mvcedu/MvceduApplication.java
@@ -0,0 +1,13 @@
+package edu.pkch.mvcedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvceduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvceduApplication.class, args);
+    }
+
+}
