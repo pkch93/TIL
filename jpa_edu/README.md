1e562eed1fd755557af15e4456a15eb360e8518c
pkch93 <star_final@naver.com>
2019-07-14 15:00:52 +0900

    TIL | 190714 0300 PM | JPA study practice environment settings

---
+++ jpa_edu/README.md
@@ -0,0 +1,17 @@
+# JPA Edu
+
+> JPA 학습을 위한 Repo
+>
+> 참고 : 자바 ORM 표준 JPA 프로그래밍 (저자: 김영한님)
+
+## Environment
+
+> 책에 나와있는 설정대로 진행
+
+module | version
+--- | ---
+:--: | :--:
+hibernate | 4.3.10.Final
+h2 | 1.4.197
+junit | 5.5.0
+
--- jpa_edu/src/main/java/edu/pkch/jpaedu/JpaMain.java
+++ jpa_edu/src/main/java/edu/pkch/jpaedu/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -24,5 +25,27 @@ public class JpaMain {
     }
 
     private static void logic(final EntityManager em) {
+        String id = "id1";
+        Member member = new Member();
+        member.setId(id);
+        member.setUsername("지한");
+        member.setAge(2);
+        //등록
+        em.persist(member);
+
+        //수정
+        member.setAge(20);
+
+        //한 건 조회
+        Member findMember = em.find(Member.class, id);
+        System.out.println("findMember=" + findMember.getUsername() + ", age=" + findMember.getAge());
+
+        //목록 조회
+        List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
+        System.out.println("members.size=" + members.size());
+
+        //삭제
+        em.remove(member);
+
     }
 }
