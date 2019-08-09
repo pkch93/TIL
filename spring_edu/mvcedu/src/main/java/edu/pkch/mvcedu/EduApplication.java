58cf1071e88d15e38845a4ec77d48708f1b1b156
pkch93 <star_final@naver.com>
2019-08-09 16:31:28 +0900

    refactor: Main Controller rename MvceduApplication to EduApplication

--- spring_edu/mvcedu/src/main/java/edu/pkch/mvcedu/EduApplication.java
+++ spring_edu/mvcedu/src/main/java/edu/pkch/mvcedu/EduApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MvceduApplication {
+public class EduApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MvceduApplication.class, args);
+        SpringApplication.run(EduApplication.class, args);
     }
 
 }
